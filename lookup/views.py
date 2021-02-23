08fad464ac23d30f6c6c9ab08105fe15dcb5f526
Galina Troshkina <maystar2009@gmail.com>
2021-02-23 19:16:32 +0300

    handled API and home view error

--- lookup/views.py
+++ lookup/views.py
@@ -2,7 +2,17 @@ from django.shortcuts import render
 
 
 def home(request):
-    return render(request, 'home.html', {})
+    import json
+    import requests
+
+    api_request = requests.get("https://www.airnowapi.org/aq/observation/zipCode/current/?format=application/json&zipCode=20002&distance=5&API_KEY=CFFEF205-1291-454F-B5DE-6D0042BC0ED6")
+    
+    try:
+        api = json.loads(api_request.content)
+    except Exception as e:
+        api = 'Error...'
+
+    return render(request, 'home.html', {'api': api})
 
 
 def about(request):
